18865224ed65678e60c1cf06be190869ba5836fb
sonik8494 <sourabh.niks1994@gmail.com>
2018-11-23 16:55:10 +0000

    Done

--- q01_create_dict/build.py
+++ q01_create_dict/build.py
@@ -1,8 +1,15 @@
+# %load q01_create_dict/build.py
 import pandas as pd
 
 path = 'data/babies_name.csv'
 data = pd.read_csv(path,names=['Name', 'Gender', 'Count', 'Year'])
 
-def q01_create_dict():
-    "write your solution here"
+
+
+def q01_create_dict(data):
+# 'write your solution here'
+
+# data.groupby('Name')['Count'].apply(list).to_dict()
+    return dict(zip(data['Name'],data['Count']))
+
 
